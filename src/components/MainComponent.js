226158b98254d91516a94e77d04512bfefb6d52a
Jay Montemayor <montemayorj@hotmail.com>
2021-09-07 10:52:39 -0700

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -38,7 +39,8 @@ class Main extends Component {
             return (
                 <CampsiteInfo 
                     campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                    comments={this.state.comments.filter(comment => comment.id === +match.params.campsiteId)} />
+                    comments={this.state.comments.filter(comment => comment.id === +match.params.campsiteId)}
+                />
             );
         }
 
@@ -50,6 +52,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
